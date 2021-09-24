34207f06520543acc473abcd584c1bbd42101716
Ratoriku <a1063021545@gmail.com>
2021-09-24 21:13:12 +0800

    tulip: Switch to AIDL Light HAL
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: xyz-mocha <xyzxmocha@gmail.com>
    Change-Id: I2618bcb81902688b9b9b975f612c653707787202

--- device.mk
+++ device.mk
@@ -285,7 +285,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.tulip
+    android.hardware.lights-service.xiaomi_tulip
 
 # Media
 PRODUCT_PACKAGES += \
